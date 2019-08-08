bea135fc61a555062367813ea3f988877ff1e974
Tuan Nguyen <tuanng@pixiostudio.com>
2019-08-08 11:59:36 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+Use App\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/create', 'ArticleController@create');
+Route::post('articles', 'ArticsleController@store');
+Route::get('articles/{id}', 'ArticleController@show');
+Route::get('articles/{id}/edit', 'ArticleController@edit');
+Route::put('articles/{id}', 'ArticleController@update');
+Route::delete('articles/{id}', 'ArticleController@destroy');
